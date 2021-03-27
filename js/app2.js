b653c392a9ea512ad4c2f4494ba908ed776b6cb7
mohammad ashour <moh.ash96@gmail.com>
2021-03-27 16:23:45 +0300

    fimished everything

--- js/app2.js
+++ js/app2.js
@@ -36,15 +36,67 @@ function compareHorn(a, b) {
 }
 
 let pictrueItems = [];
+
 const ajaxSettings = {
     method: 'get',
     dataType: 'json'
 };
 
+function sorting() {
+    $("input[type='radio'").click(function () {
+        let radioc = $("input[name='sort']:checked").val();
+        if (radioc == 'title') {
+            $('#picSection').empty();
+            pictrueItems.sort(compare);
+
+            pictrueItems.forEach(newObj => {
+
+                const template = $('#template').html();
+
+                const html = Mustache.render(template, newObj);
+
+                $('#picSection').append(html);
+            });
+
+        } else if (radioc == 'horns') {
+            $('#picSection').empty();
+            pictrueItems.sort(compareHorn);
+
+            pictrueItems.forEach(newObj => {
+
+                const template = $('#template').html();
+
+                const html = Mustache.render(template, newObj);
+
+                $('#picSection').append(html);
+            });
+
+        }
+    })
+}
 
 
 
 $(function () {
+    $.ajax('data/page-1.json', ajaxSettings).then(data => {
+        data.forEach(items => {
+
+            let newItem = new Picture(items);
+            pictrueItems.push(newItem);
+
+            if (filters.includes(items.keyword) == false) {
+                filters.push(items.keyword);
+                $('#my-select').append(`<option value = "${items.keyword}" class = "opt">${items.keyword}</option>`);
+            };
+            const template = $('#template').html();
+
+            const html = Mustache.render(template, items);
+
+            $('#picSection').append(html);
+        });
+
+        sorting();
+    })
     $('#b1').on('click', (event) => {
         event.preventDefault();
         filters = [];
@@ -68,37 +120,7 @@ $(function () {
                 $('#picSection').append(html);
             });
 
-            $("input[type='radio'").click(function () {
-                let radioc = $("input[name='sort']:checked").val();
-                console.log(radioc);
-                if(radioc == 'title'){
-                    $('#picSection').empty();
-                    pictrueItems.sort(compare);
-
-                    pictrueItems.forEach(newObj => {
-        
-                        const template = $('#template').html();
-        
-                        const html = Mustache.render(template, newObj);
-        
-                        $('#picSection').append(html);
-                    });
-                    
-                } else if(radioc == 'horns'){
-                    $('#picSection').empty();
-                    pictrueItems.sort(compareHorn);
-
-                    pictrueItems.forEach(newObj => {
-        
-                        const template = $('#template').html();
-        
-                        const html = Mustache.render(template, newObj);
-        
-                        $('#picSection').append(html);
-                    });
-
-                }
-            })
+            sorting();
 
         });
 
@@ -129,38 +151,9 @@ $(function () {
 
                 $('#picSection').append(html);
 
-            })
-            $("input[type='radio'").click(function () {
-                let radioc = $("input[name='sort']:checked").val();
-                console.log(radioc);
-                if(radioc == 'title'){
-                    $('#picSection').empty();
-                    pictrueItems.sort(compare);
+            });
 
-                    pictrueItems.forEach(newObj => {
-        
-                        const template = $('#template').html();
-        
-                        const html = Mustache.render(template, newObj);
-        
-                        $('#picSection').append(html);
-                    });
-                    
-                } else if(radioc == 'horns'){
-                    $('#picSection').empty();
-                    pictrueItems.sort(compareHorn);
-
-                    pictrueItems.forEach(newObj => {
-        
-                        const template = $('#template').html();
-        
-                        const html = Mustache.render(template, newObj);
-        
-                        $('#picSection').append(html);
-                    });
-
-                }
-            })
+            sorting();
 
         })
     })
---
+++ js/app3.js
@@ -0,0 +1,140 @@
+'use strict'
+
+let filters = [];
+
+function Picture(items) {
+    for (let i in items) {
+        this[i] = items[i];
+    }
+}
+
+
+function compare(a, b) {
+    const picA = a.title.toUpperCase();
+    const picB = b.title.toUpperCase();
+
+    let comparison = 0;
+    if (picA > picB) {
+        comparison = 1;
+    } else if (picA < picB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareHorn(a, b) {
+    const picA = a.horns;
+    const picB = b.horns;
+
+    let comparison = 0;
+    if (picA > picB) {
+        comparison = 1;
+    } else if (picA < picB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+let pictrueItems = [];
+
+function render (data){
+    data.forEach(items => {
+            
+        let newItem = new Picture(items);
+        pictrueItems.push(newItem);
+        
+    if (filters.includes(items.keyword) == false) {
+        filters.push(items.keyword);
+        $('#my-select').append(`<option value = "${items.keyword}" class = "opt">${items.keyword}</option>`);
+    };
+    const template = $('#template').html();
+
+    const html = Mustache.render(template, items);
+    
+    $('#picSection').append(html);
+});
+}
+
+const ajaxSettings = {
+    method: 'get',
+    dataType: 'json'
+};
+
+
+$(function () {
+    $.ajax('data/page-1.json', ajaxSettings).then(data => {
+        data.forEach(items => {
+            
+            let newItem = new Picture(items);
+            pictrueItems.push(newItem);
+            
+        if (filters.includes(items.keyword) == false) {
+            filters.push(items.keyword);
+            $('#my-select').append(`<option value = "${items.keyword}" class = "opt">${items.keyword}</option>`);
+        };
+        const template = $('#template').html();
+
+        const html = Mustache.render(template, items);
+        
+        $('#picSection').append(html);
+    });
+})
+    $('#b1').on('click', (event) => {
+        event.preventDefault();
+        filters = [];
+        pictrueItems = [];
+        $('.opt').remove();
+        $('#picSection').empty();
+        $.ajax('data/page-1.json', ajaxSettings).then(data => {
+            data.forEach(items => {
+
+                let newItem = new Picture(items);
+                pictrueItems.push(newItem);
+
+                if (filters.includes(items.keyword) == false) {
+                    filters.push(items.keyword);
+                    $('#my-select').append(`<option value = "${items.keyword}" class = "opt">${items.keyword}</option>`);
+                };
+                const template = $('#template').html();
+
+                const html = Mustache.render(template, items);
+
+                $('#picSection').append(html);
+            });
+
+            $("input[type='radio'").click(function () {
+                let radioc = $("input[name='sort']:checked").val();
+                console.log(radioc);
+                if(radioc == 'title'){
+                    $('#picSection').empty();
+                    pictrueItems.sort(compare);
+
+                    pictrueItems.forEach(newObj => {
+        
+                        const template = $('#template').html();
+        
+                        const html = Mustache.render(template, newObj);
+        
+                        $('#picSection').append(html);
+                    });
+                    
+                } else if(radioc == 'horns'){
+                    $('#picSection').empty();
+                    pictrueItems.sort(compareHorn);
+
+                    pictrueItems.forEach(newObj => {
+        
+                        const template = $('#template').html();
+        
+                        const html = Mustache.render(template, newObj);
+        
+                        $('#picSection').append(html);
+                    });
+
+                }
+            })
+
+        });
+
+    });
+});
